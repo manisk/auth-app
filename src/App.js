0dc46bf11da231d9dbe18a5c24f17301773817d2
Manish <manisk.kumar38@gmail.com>
2022-01-22 21:11:04 +0530

    changed to hash router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import Home from "./Component/Home/Home";
 import Login from "./Component/Login/Login";
 import Register from "./Component/Register/Register";
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
